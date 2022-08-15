0370fe38614b3afb121fbf25a6f73e2331e51af1
Ramiro <ramifazio@gmail.com>
2022-08-15 12:50:20 -0300

    react-router-dom config ready

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+//CSS
 import styled from "styled-components";
 import GlobalStyles from "../../Styles/GlobalStyles";
+//Assets
 import homeImg from "../../Assets/Pics/IMG_9728.jpg";
 
 const StyledImg = styled.img`
