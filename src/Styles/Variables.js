c6b7bef207eaced3d5fd4d81a6f65bb4ded00823
Ramiro <ramifazio@gmail.com>
2022-08-30 12:57:18 -0300

    Add responsive Variables

--- src/Styles/Variables.js
+++ src/Styles/Variables.js
@@ -13,6 +13,12 @@ const Variables = {
   emphasysColor: "#000000",
   navBarColor: "#e1d6cd",
   navBarTransparentColor: "#e1d6cd51",
+
+  //Responsive
+  mobileS: `max-width: 400px`,
+  mobileL: `max-width: 500px`,
+  tabletL: `max-width: 768px`,
+  desktopXS: `max-width: 900px`,
 };
 
 export default Variables;
