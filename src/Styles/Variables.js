a4375efa7c6734765d8724a7e918df658121a6ba
Ramiro <ramifazio@gmail.com>
2022-08-30 12:59:31 -0300

    Details

--- src/Styles/Variables.js
+++ src/Styles/Variables.js
@@ -15,10 +15,10 @@ const Variables = {
   navBarTransparentColor: "#e1d6cd51",
 
   //Responsive
-  mobileS: `max-width: 400px`,
-  mobileL: `max-width: 500px`,
-  tabletL: `max-width: 768px`,
-  desktopXS: `max-width: 900px`,
+  mobileS: "max-width: 400px",
+  mobileL: "max-width: 500px",
+  tabletL: "max-width: 768px",
+  desktopXS: "max-width: 900px",
 };
 
 export default Variables;
