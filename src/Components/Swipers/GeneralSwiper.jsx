84d9487b243f84a5a096f19c628e956ba446feb4
Ramiro <ramifazio@gmail.com>
2022-09-05 12:48:49 -0300

    Responsive Swiper

--- src/Components/Swipers/GeneralSwiper.jsx
+++ src/Components/Swipers/GeneralSwiper.jsx
@@ -1,33 +1,70 @@
 import React from "react";
 //Assets
 import styled from "styled-components";
+import Variables from "../../Styles/Variables";
 //SwiperJS
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper";
 
 function GeneralSwiper({ images }) {
-  return (
-    <SwiperContainer
-      autoplay={{
-        delay: 4000,
-        disableOnInteraction: false,
-      }}
-      effect={"fade"}
-      slidesPerView={"auto"}
-      loop={true}
-      centeredSlides={true}
-      pagination={{ clickable: true, dynamicBullets: true }}
-      modules={[Pagination, Autoplay]}
-    >
-      {images.map((item, index) => (
-        <SwiperSlide style={{ width: "75%" }} key={index}>
-          <Image src={item} />
-        </SwiperSlide>
-      ))}
-    </SwiperContainer>
-  );
+  const width = window.innerWidth;
+
+  if (width < 426) {
+    return (
+      <SwiperContainer
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        direction={"vertical"}
+        effect={"fade"}
+        slidesPerView={"auto"}
+        spaceBetween={0}
+        loop={true}
+        centeredSlides={true}
+        pagination={{ clickable: true, dynamicBullets: true }}
+        modules={[Pagination, Autoplay]}
+      >
+        {images.map((item, index) => (
+          <SwiperSlide
+            style={{
+              display: "flex",
+              alignItems: "center",
+              width: "100%",
+              height: "50%",
+            }}
+            key={index}
+          >
+            <Image src={item} />
+          </SwiperSlide>
+        ))}
+      </SwiperContainer>
+    );
+  } else {
+    return (
+      <SwiperContainer
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        effect={"fade"}
+        slidesPerView={"auto"}
+        loop={true}
+        centeredSlides={true}
+        pagination={{ clickable: true, dynamicBullets: true }}
+        modules={[Pagination, Autoplay]}
+      >
+        {images.map((item, index) => (
+          <SwiperSlide style={{ width: "75%" }} key={index}>
+            <Image src={item} />
+          </SwiperSlide>
+        ))}
+      </SwiperContainer>
+    );
+  }
 }
 
 export default GeneralSwiper;
@@ -44,4 +81,15 @@ const Image = styled.img`
   width: 100%;
   height: 90%;
   margin-top: 10%;
+
+  @media (${Variables.tabletL}) {
+    height: 85%;
+    margin-top: 15%;
+  }
+
+  @media (${Variables.mobileL}) {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
 `;
