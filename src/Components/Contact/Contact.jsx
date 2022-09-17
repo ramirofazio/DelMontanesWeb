bcc0edb287bf1be967b3b9d21f2623318648c1e1
Ramiro <ramifazio@gmail.com>
2022-09-17 17:08:06 -0300

    First style of the form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,10 +3,13 @@ import styled from "styled-components";
 import GlobalStyles from "../../Styles/GlobalStyles";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import Variables from "../../Styles/Variables";
 
 function Alfajores() {
   return (
     <StyledContainer>
+      <StyledSubContainer></StyledSubContainer>
+
       <Formik
         initialValues={{
           nombre: "",
@@ -22,35 +25,65 @@ function Alfajores() {
           }, 400);
         }}
         validationSchema={Yup.object({
-          nombre: Yup.string().max(15).required("Requerido"),
-          apellido: Yup.string().max(15).required("Requerido"),
-          email: Yup.string().email().required("Requerido"),
-          telefono: Yup.number().max(15).required("Requerido"),
-          consulta: Yup.string().max(200).required("Requerido"),
+          nombre: Yup.string().max(15).required("*"),
+          apellido: Yup.string().max(15).required("*"),
+          email: Yup.string().email().required("*"),
+          telefono: Yup.number().max(15).required("*"),
+          consulta: Yup.string().max(200).required("*"),
         })}
       >
         <StyledForm>
-          <StyledLabel htmlFor="nombre">nombre</StyledLabel>
-          <StyledField name="nombre" type="text" />
-          <ErrorMessage name="nombre" />
+          <Title>CONSULTAS O SUGERENCIAS</Title>
+          <StyledRow>
+            <StyledInputContainer>
+              <ErrorMessage
+                name="nombre"
+                render={(msg) => <StyledErrorMsj>{msg}</StyledErrorMsj>}
+              />
+              <StyledLabel htmlFor="nombre">Nombre</StyledLabel>
+              <StyledField name="nombre" type="text" />
+            </StyledInputContainer>
 
-          <StyledLabel htmlFor="apellido">apellido</StyledLabel>
-          <StyledField name="apellido" type="text" />
-          <ErrorMessage name="apellido" />
+            <StyledInputContainer>
+              <StyledLabel htmlFor="apellido">Apellido</StyledLabel>
+              <StyledField name="apellido" type="text" />
+              <ErrorMessage
+                name="apellido"
+                render={(msg) => <StyledErrorMsj>{msg}</StyledErrorMsj>}
+              />
+            </StyledInputContainer>
+          </StyledRow>
 
-          <StyledLabel htmlFor="email">email</StyledLabel>
-          <StyledField name="email" type="text" />
-          <ErrorMessage name="email" />
+          <StyledRow>
+            <StyledInputContainer>
+              <StyledLabel htmlFor="email">Email</StyledLabel>
+              <StyledField name="email" type="email" />
+              <ErrorMessage
+                name="email"
+                render={(msg) => <StyledErrorMsj>{msg}</StyledErrorMsj>}
+              />
+            </StyledInputContainer>
 
-          <StyledLabel htmlFor="telefono">telefono</StyledLabel>
-          <StyledField name="telefono" type="text" />
-          <ErrorMessage name="telefono" />
+            <StyledInputContainer>
+              <StyledLabel htmlFor="telefono">Telefono</StyledLabel>
+              <StyledField name="telefono" type="number" />
+              <ErrorMessage
+                name="telefono"
+                render={(msg) => <StyledErrorMsj>{msg}</StyledErrorMsj>}
+              />
+            </StyledInputContainer>
+          </StyledRow>
 
-          <StyledLabel htmlFor="consulta">consulta</StyledLabel>
-          <StyledField name="consulta" type="text" />
-          <ErrorMessage name="consulta" />
+          <StyledLabel htmlFor="consulta">Consulta</StyledLabel>
+          <StyledTextArea name="consulta" type="text" as={"textarea"} />
+          <ErrorMessage
+            name="consulta"
+            render={(msg) => <StyledErrorMsj>{msg}</StyledErrorMsj>}
+          />
 
-          <button type="submit">Submit</button>
+          <StyledBtnContainer>
+            <StyledBtn type="submit">ENVIAR MENSAJE</StyledBtn>
+          </StyledBtnContainer>
         </StyledForm>
       </Formik>
     </StyledContainer>
@@ -61,6 +94,13 @@ export default Alfajores;
 
 const StyledContainer = styled.div`
   ${GlobalStyles.container}
+  background-color: ${Variables.baseColor};
+`;
+
+const StyledSubContainer = styled.div`
+  flex: 1;
+  height: 100%;
+  background-color: ${Variables.secondaryColor};
 `;
 
 const StyledForm = styled(Form)`
@@ -68,15 +108,100 @@ const StyledForm = styled(Form)`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 50%;
-  height: 70%;
+  width: 30%;
+  padding-inline: 50px;
+  height: 100%;
+`;
+
+const Title = styled.h1`
+  color: ${Variables.principalColor};
+  font-size: 35px;
+  font-family: ${Variables.secondaryFont};
+  width: auto;
+  text-align: center;
+  margin: 0;
+  margin-bottom: 10%;
+  padding: 0;
+  border-bottom: 1px solid ${Variables.principalColor};
+`;
+
+const StyledRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 10%;
+`;
+
+const StyledInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  height: 100%;
+  align-items: flex-start;
+  justify-content: center;
 `;
 
 const StyledLabel = styled.label`
-  color: red;
+  color: ${Variables.principalColor};
+  margin-left: 10px;
+  margin-bottom: 3px;
 `;
 
 const StyledField = styled(Field)`
+  height: 40%;
   width: 100%;
-  flex: 0.5;
+
+  border: none;
+  border-radius: 5px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const StyledTextArea = styled(Field)`
+  height: 10%;
+  min-height: 10%;
+  max-height: 30%;
+  width: 100%;
+  min-width: 100%;
+  max-width: 100%;
+
+  border: none;
+  border-radius: 4px;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const StyledErrorMsj = styled.p`
+  color: red;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  margin-top: -28px;
+`;
+
+const StyledBtnContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 10%;
+`;
+
+const StyledBtn = styled.button`
+  ${GlobalStyles.button}
+  height: 40%;
+  width: 40%;
+  font-family: ${Variables.secondaryFont};
+
+  border: none;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
